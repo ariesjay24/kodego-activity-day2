e48c7b8b82998ce23f24cae27bd48e93574223d2
ariesjay24 <ariescuevas063@gmail.com>
2023-09-05 15:56:35 +0800

    final commit

--- index.js
+++ index.js
@@ -4,3 +4,26 @@ const header = document.querySelector("header");
 menu.addEventListener("click", function () {
   header.classList.toggle("open");
 });
+
+let a = document.getElementById("loginBtn");
+let b = document.getElementById("registerBtn");
+let x = document.getElementById("login");
+let y = document.getElementById("register");
+
+function login() {
+  x.style.left = "4px";
+  y.style.right = "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+}
+
+function register() {
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1;
+}
